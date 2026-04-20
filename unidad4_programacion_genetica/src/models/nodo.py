acb34f961458b4aa66b00622d091862290867311
Miguel Angel Lara Tarin <lara.miguel@uabc.edu.mx>
2026-04-20 09:58:01 -0700

    Implemente la clase Nodo

---
+++ unidad4_programacion_genetica/src/models/nodo.py
@@ -0,0 +1,39 @@
+import numpy as np
+class Nodo:
+    def __init__(self, valor, izquierda = None, derecha = None):
+        self.valor = valor
+        self.izquierda = izquierda
+        self.derecha = derecha
+
+    def evaluar(self, X):
+        if self.izquierda is None and self.derecha is None:
+            if isinstance(self.valor, str) and self.valor.startswith('X'):
+                return X[:, int(self.valor[1:])]
+            return self.valor
+        
+        izq_val = self.izquierda.evaluar(X) if self.izquierda else None
+        der_val = self.derecha.evaluar(X) if self.derecha else None
+
+        if self.valor == '+':
+            return izq_val + der_val
+        elif self.valor == '-':
+            return izq_val - der_val
+        elif self.valor == '*':
+            return izq_val * der_val
+        elif self.valor == '/':
+            condicion = np.abs(der_val) < 1e-10
+            der_val_seguro = np.where(condicion, 1.0, der_val)
+            resultado = izq_val / der_val_seguro
+            return np.where(condicion, 1.0, resultado)
+        elif self.valor == 'sin':
+            return np.sin(izq_val)
+        elif self.valor == 'cos':
+            return np.cos(izq_val)
+        elif self.valor == 'log':
+            val_abs = np.abs(izq_val)
+            val_seguro = np.where(val_abs == 0, 1e-10, val_abs)
+            return np.log(val_seguro)
+        elif self.valor == 'sqrt':
+            return np.sqrt(np.abs(izq_val))
+        elif self.valor == 'exp':
+            return np.exp(izq_val)
